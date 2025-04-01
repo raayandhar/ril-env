0b04876485920dfd519476de7cfd6c01f2128079
raayan.dhar@gmail.com <raayan.dhar@gmail.com>
2025-03-31 21:13:44 -0700

    Temporary workaround for the homing problem.

--- ril_env/xarm_controller.py
+++ ril_env/xarm_controller.py
@@ -34,6 +34,205 @@ class XArmConfig:
     verbose: bool = True
 
 
+class XArm:
+    """
+    At this point, this is legacy code. If you want to record
+    data, please use the multiprocessing-emabled XArmController.
+    """
+
+    def __init__(self, xarm_config: XArmConfig):
+        self.config = xarm_config
+        self.init = False
+
+        self.current_position = None
+        self.current_orientation = None
+        self.previous_grasp = 0.0
+
+        if self.config.verbose:
+            logger.setLevel(logging.DEBUG)
+
+    @property
+    def is_ready(self):
+        return self.init
+
+    def initialize(self):
+        self.arm = XArmAPI(self.config.robot_ip)
+        arm = self.arm
+
+        arm.connect()
+        arm.clean_error()
+        arm.clean_warn()
+
+        code = arm.motion_enable(enable=True)
+        if code != 0:
+            logger.error(f"Error in motion_enable: {code}")
+            raise RuntimeError(f"Error in motion_enable: {code}")
+
+        arm.set_tcp_maxacc(self.config.tcp_maxacc)
+
+        code = arm.set_mode(1)
+        if code != 0:
+            logger.error(f"Error in set_mode: {code}")
+            raise RuntimeError(f"Error in set_mode: {code}")
+
+        code = arm.set_state(0)
+        if code != 0:
+            logger.error(f"Error in set_state: {code}")
+            raise RuntimeError(f"Error in set_state: {code}")
+
+        code, state = arm.get_state()
+        if code != 0:
+            logger.error(f"Error getting robot state: {code}")
+            raise RuntimeError(f"Error getting robot state: {code}")
+        if state != 0:
+            logger.error(f"Robot is not ready to move. Current state: {state}")
+            raise RuntimeError(f"Robot is not ready to move. Current state: {state}")
+        else:
+            logger.info(f"Robot is ready to move. Current state: {state}")
+
+        err_code, warn_code = arm.get_err_warn_code()
+        if err_code != 0 or warn_code != 0:
+            logger.error(
+                f"Error code: {err_code}, Warning code: {warn_code}. Cleaning error and warning."
+            )
+            arm.clean_error()
+            arm.clean_warn()
+            arm.motion_enable(enable=True)
+            arm.set_state(0)
+
+        code = arm.set_gripper_mode(0)
+        if code != 0:
+            logger.error(f"Error in set_gripper_mode: {code}")
+            raise RuntimeError(f"Error in set_gripper_mode: {code}")
+
+        code = arm.set_gripper_enable(True)
+        if code != 0:
+            logger.error(f"Error in set_gripper_enable: {code}")
+            raise RuntimeError(f"Error in set_gripper_enable: {code}")
+
+        code = arm.set_gripper_speed(1000)
+        if code != 0:
+            logger.error(f"Error in set_gripper_speed: {code}")
+            raise RuntimeError(f"Error in set_gripper_speed: {code}")
+
+        self.init = True
+        time.sleep(3)
+        self.home()
+        time.sleep(3)
+        logger.info("Successfully initialized xArm.")
+
+    def shutdown(self):
+        if not self.init:
+            logger.error("shutdown() called on an uninitialized xArm.")
+            return
+        self.home()
+        self.arm.disconnect()
+        logger.info("xArm shutdown complete.")
+
+    def home(self):
+        logger.info("Homing robot.")
+        if not self.init:
+            logger.error("xArm not initialized.")
+            raise RuntimeError("xArm not initialized.")
+
+        arm = self.arm
+        arm.set_mode(0)
+        arm.set_state(0)
+        code = arm.set_gripper_position(850, wait=False)
+        if code != 0:
+            logger.error(f"Error in set_gripper_position (open, homing): {code}")
+            raise RuntimeError(f"Error in set_gripper_position (open, homing): {code}")
+
+        code = arm.set_servo_angle(
+            angle=self.config.home_pos, speed=self.config.home_speed, wait=True
+        )
+        if code != 0:
+            logger.error(f"Error in set_servo_angle (homing): {code}")
+            raise RuntimeError(f"Error in set_servo_angle (homing): {code}")
+        arm.set_mode(1)
+        arm.set_state(0)
+
+        code, pose = arm.get_position()
+        if code != 0:
+            logger.error(f"Failed to query initial pose: {code}")
+            raise RuntimeError(f"Failed to query initial pose: {code}")
+        else:
+            self.current_position = np.array(pose[:3])
+            self.current_orientation = np.array(pose[3:])
+            logger.debug(
+                f"Initial pose set: pos={self.current_position}, ori={self.current_orientation}"
+            )
+
+    def step(self, dpos, drot, grasp):
+        if not self.init:
+            logger.error("xArm not initialized. Use it in a 'with' block")
+            raise RuntimeError("xArm not initialized. Use it in a 'with' block")
+
+        dpos *= self.config.position_gain
+        drot *= self.config.orientation_gain
+
+        curr_rot = st.Rotation.from_euler("xyz", self.current_orientation, degrees=True)
+        delta_rot = st.Rotation.from_euler("xyz", drot, degrees=True)
+        final_rot = delta_rot * curr_rot
+
+        self.current_orientation = final_rot.as_euler("xyz", degrees=True)
+        self.current_position += dpos
+
+        logger.debug(f"Current position: {self.current_position}")
+        logger.debug(f"Current orientation: {self.current_orientation}")
+
+        code = self.arm.set_servo_cartesian(
+            np.concatenate((self.current_position, self.current_orientation)),
+            is_radian=False,
+        )
+        if code != 0:
+            logger.error(f"Error in set_servo_cartesian in step(): {code}")
+            raise RuntimeError(f"Error in set_servo_cartesian in step(): {code}")
+
+        if grasp != self.previous_grasp:
+            if grasp == 1.0:
+                code = self.arm.set_gripper_position(0, wait=False)
+                if code != 0:
+                    logger.error(f"Error in set_gripper_position (close): {code}")
+                    raise RuntimeError(f"Error in set_gripper_position (close): {code}")
+            else:
+                code = self.arm.set_gripper_position(850, wait=False)
+                if code != 0:
+                    logger.error(f"Error in set_gripper_position (open): {code}")
+                    raise RuntimeError(f"Error in set_gripper_position (open): {code}")
+            self.previous_grasp = grasp
+
+    def get_state(self):
+        state = {}
+
+        code, actual_pose = self.arm.get_position(is_radian=False)
+        if code != 0:
+            logger.error(f"Error getting TCP pose: code {code}")
+            raise RuntimeError(f"Error getting TCP pose: code {code}")
+        state["ActualTCPPose"] = actual_pose
+
+        actual_tcp_speed = self.arm.realtime_tcp_speed()
+        state["ActualTCPSpeed"] = actual_tcp_speed
+
+        code, actual_angles = self.arm.get_servo_angle(is_radian=False)
+        if code != 0:
+            logger.error(f"Error getting joint angles: code {code}")
+            raise RuntimeError(f"Error getting joint angles: code {code}")
+        state["ActualQ"] = actual_angles
+
+        actual_joint_speeds = self.arm.realtime_joint_speeds()
+        state["ActualQd"] = actual_joint_speeds
+
+        return state
+
+    def __enter__(self):
+        self.initialize()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.shutdown()
+
+
 class Command(enum.Enum):
     STOP = 0
     STEP = 1
@@ -236,6 +435,8 @@ class XArmController(mp.Process):
                         print("HELLO AGAIN")
                         logger.debug(f"[XArmController] New target pose: {target_pose}")
                     elif cmd == Command.HOME.value:
+                        # Currently, there are some issues here. It is best to move closer
+                        # to home before homing, otherwise it is *very* dangerous.
                         logger.info("[XArmController] Received HOME command.")
                         arm.set_mode(0)
                         arm.set_state(0)
@@ -373,6 +574,8 @@ def main():
 
                 # Right button -> HOME
                 # DO NOT MOVE THE SPACEMOUSE WHILE CLICKING THIS.
+                # The HOME command is buggy and best avoided...
+                """
                 if sm.is_button_pressed(1):
                     command = {
                         "cmd": Command.HOME.value,
@@ -388,6 +591,7 @@ def main():
                     new_pose = updated_state["TCPPose"]
                     current_target_pose[:] = new_pose
                     continue
+                """
 
                 dpos *= xarm_ctrl.position_gain
                 drot *= xarm_ctrl.orientation_gain
@@ -399,6 +603,19 @@ def main():
 
                 current_target_pose[:3] += dpos
                 current_target_pose[3:] = final_rot.as_euler("xyz", degrees=True)
+
+                # This is a workaround that does not use home_pos
+                # This will have to be fixed later.
+                # This is also like insanely dangerous...
+                if sm.is_button_pressed(1):
+                    current_target_pose = [
+                        475.791901,
+                        -1.143693,
+                        244.719421,
+                        179.132906,
+                        -0.010084,
+                        0.77567,
+                    ]
 
                 command = {
                     "cmd": Command.STEP.value,
